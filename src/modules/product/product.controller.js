232b29e603cff5298a7508c08eae77d026e4b927
iAmSarvar <iamsarvarsultanov@gmail.com>
2026-02-20 22:50:20 +0500

    feat(product): implement CRUD endpoints with async error handling

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -1,17 +1,57 @@
 import { catchAsync } from "../../utils/catchAsync.js";
-
-const listProducts = catchAsync(async (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "list products",
-  });
-});
+import productService from "./product.service.js";
+import { AppError } from "../../utils/AppError.js";
 
 const createProduct = catchAsync(async (req, res) => {
+  const product = await productService.createProduct(req.body);
+
   res.status(201).json({
     status: "success",
-    message: "create product",
+    data: product,
   });
 });
 
-export default { listProducts, createProduct };
+const getAllProducts = catchAsync(async (req, res) => {
+  const products = await productService.getAllProducts();
+
+  res.status(200).json({
+    status: "success",
+    results: products.length,
+    data: products,
+  });
+});
+
+const getProduct = catchAsync(async (req, res, next) => {
+  const product = await productService.getProductById(req.params.id);
+
+  if (!product) return next(new AppError("Product not found!", 404));
+
+  res.status(200).json({
+    status: "success",
+    data: product,
+  });
+});
+
+const updateProduct = catchAsync(async (req, res, next) => {
+  const product = await productService.updateProductById(req.params.id, req.body);
+
+  if (!product) return next(new AppError("Product not found!", 404));
+
+  res.status(200).json({
+    status: "success",
+    data: product,
+  });
+});
+
+const deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await productService.deleteProductById(req.params.id);
+
+  if (!product) return next(new AppError("Product not found", 404));
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+export default { createProduct, getAllProducts, getProduct, updateProduct, deleteProduct };
--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -3,6 +3,12 @@ import productController from "./product.controller.js";
 
 const router = express.Router();
 
-router.route("/").get(productController.listProducts).post(productController.createProduct);
+router.route("/").get(productController.getAllProducts).post(productController.createProduct);
+
+router
+  .route("/:id")
+  .get(productController.getProduct)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
 
 export default router;
